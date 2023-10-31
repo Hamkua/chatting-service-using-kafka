379559c849989e0e62fbab4d80acc9a2963a61dd
함궈 <84824124+Hamkua@users.noreply.github.com>
2023-10-31 21:01:41 +0900

    test: 워커 삭제 테스트 코드 작성

--- src/test/java/com/hamkua/chattingserviceusingkafka/chatting/ConsumerManagerTest.java
+++ src/test/java/com/hamkua/chattingserviceusingkafka/chatting/ConsumerManagerTest.java
@@ -1,7 +1,10 @@
 package com.hamkua.chattingserviceusingkafka.chatting;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,9 +19,13 @@ class ConsumerManagerTest {
     @Autowired
     private ConsumerManager consumerManager;
 
+    Logger log = LoggerFactory.getLogger(ConsumerManagerTest.class.getSimpleName());
+
     @Test
     @Order(1)
+    @DisplayName("워커 생성 테스트")
     void addConsumerWorkerTest() {
+        log.info("addConsumerWorkerTest");
 
         //given
         Long chattingRoomId = 1L;
@@ -29,12 +36,11 @@ class ConsumerManagerTest {
 
         //then
         assertTrue(isAdded);
-
     }
-
 
     @Test
     @Order(2)
+    @DisplayName("워커 조회 테스트")
     void getConsumerWorkerTest(){
 
         //given
@@ -48,4 +54,22 @@ class ConsumerManagerTest {
         //then
         assertEquals(expectedThreadName, worker.getThreadName());
     }
+
+    @Test
+    @Order(3)
+    @DisplayName("워커 삭제 테스트")
+    void subConsumerWorkerTest() {
+
+        //given
+        Long chattingRoomId = 1L;
+        Long userId = 1L;
+
+        //when
+        Boolean isRemoved = consumerManager.subConsumerWorker(chattingRoomId, userId);
+
+        //then
+        assertTrue(isRemoved);
+    }
+
+
 }
