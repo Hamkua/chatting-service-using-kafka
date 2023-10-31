4d8d68c15f9af691397c6063a1291d26fd18a319
함궈 <84824124+Hamkua@users.noreply.github.com>
2023-10-31 22:13:38 +0900

    test: 채팅방 관련 테이블 행 삽입 테스트 코드 작성

---
+++ src/test/java/com/hamkua/chattingserviceusingkafka/chatting/repository/ChattingDaoTest.java
@@ -0,0 +1,62 @@
+package com.hamkua.chattingserviceusingkafka.chatting.repository;
+
+import com.hamkua.chattingserviceusingkafka.chatting.service.ChattingService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ChattingDaoTest {
+
+    Logger log = LoggerFactory.getLogger(ChattingDaoTest.class.getSimpleName());
+
+    @Autowired
+    private ChattingDao chattingDao;
+
+    @Test
+    void findAllChattingRoomTest() {
+
+    }
+
+    @Test
+    void findAllChattingRoomByUserIdTest() {
+    }
+
+    @Test
+    @DisplayName("CHATTING_ROOM 테이블 삽입 테스트")
+    void createChattingRoomTest() {
+
+        //when
+        Long chattingRoomId = chattingDao.createChattingRoom();
+
+        //then
+//        log.info(chattingRoomId.toString());
+        assertNotNull(chattingRoomId);
+    }
+
+    @Test
+    @DisplayName("CHATTING_ROOM_USER 테이블 삽입 테스트")
+    void createChattingRoomUserTest() {
+
+        //given
+        Long chattingRoomId = Long.MAX_VALUE;
+        Long userId = Long.MAX_VALUE;
+
+        //when
+        Map<String, Long> keys = chattingDao.createChattingRoomUser(chattingRoomId, userId);
+
+        //then
+        assertEquals(2, keys.size());
+
+    }
+}
