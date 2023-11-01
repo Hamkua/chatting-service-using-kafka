8a85886d6cec769b0d0ab45955949c760470f6b1
함궈 <84824124+Hamkua@users.noreply.github.com>
2023-11-01 13:56:54 +0900

    feat: 채팅방 생성 시 브로커에 토픽을 추가하는 기능 추가

--- src/main/java/com/hamkua/chattingserviceusingkafka/chatting/service/ChattingService.java
+++ src/main/java/com/hamkua/chattingserviceusingkafka/chatting/service/ChattingService.java
@@ -1,6 +1,7 @@
 package com.hamkua.chattingserviceusingkafka.chatting.service;
 
 import com.hamkua.chattingserviceusingkafka.chatting.ConsumerManager;
+import com.hamkua.chattingserviceusingkafka.chatting.KafkaService;
 import com.hamkua.chattingserviceusingkafka.chatting.dto.ChattingRoomDto;
 import com.hamkua.chattingserviceusingkafka.chatting.dto.ChattingRoomUserDto;
 import com.hamkua.chattingserviceusingkafka.chatting.repository.ChattingDao;
@@ -19,6 +20,7 @@ public class ChattingService {
     Logger log = LoggerFactory.getLogger(ChattingService.class.getSimpleName());
 
     private final ChattingDao chattingDao;
+    private final KafkaService kafkaService;
     private final ConsumerManager consumerManager;
 
 //    유저가 소속된 채팅방 수 만큼 워커 생성 - 처음
@@ -32,6 +34,7 @@ public class ChattingService {
 
     public Boolean joinChattingRoom(Long chattingRoomId, Long userId){
         if(!chattingDao.existsChattingRoom(chattingRoomId)){
+
             log.info("해당 채팅방이 존재하지 않음. 채팅방 번호 : {}", chattingRoomId);
             return false;
         }
@@ -44,8 +47,15 @@ public class ChattingService {
 
     public Boolean createChattingRoom(List<Long> userIds){
         Long chattingRoomId = chattingDao.createChattingRoom();
+        log.info("채팅방 번호 : {}", chattingRoomId);
+
+        boolean isCreated = kafkaService.createTopic("test" + chattingRoomId);
+        if(!isCreated){
+            throw new RuntimeException("토픽 생성 실패");
+        }
 
         for(Long userId : userIds) {
+
             Map<String, Long> compositeKeys = chattingDao.createChattingRoomUser(chattingRoomId, userId);
             Boolean isAdded = consumerManager.addConsumerWorker(chattingRoomId, userId);
 
